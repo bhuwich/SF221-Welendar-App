2c995fd7427900d416764dafe4d6d99eea52df5d
Thanachot Wongmetin <32594801+ThanachotD@users.noreply.github.com>
2022-04-13 15:19:13 +0700

    Atomic Design Structure of Project Welendar

--- App.js
+++ App.js
@@ -1,27 +1,21 @@
 import React from 'react';
 import {Box, Center, NativeBaseProvider} from 'native-base';
 
-const App = () => {
-  return (
-      <Box
-        alignSelf="center"
-        bg="primary.500"
-        _text={{
-          fontSize: 'md',
-          fontWeight: 'medium',
-          color: 'warmGray.50',
-          letterSpacing: 'lg',
-        }}>
-        This is a Box
-      </Box>
-  );
-};
-
 export default () => {
   return (
     <NativeBaseProvider>
       <Center flex={1} px="3">
-        <App />
+        <Box
+          alignSelf="center"
+          bg="primary.500"
+              _text={{
+                  fontSize: 'md',
+                  fontWeight: 'medium',
+                  color: 'warmGray.50',
+                  letterSpacing: 'lg',
+              }}>
+              This is a Box
+          </Box>
       </Center>
     </NativeBaseProvider>
   );
