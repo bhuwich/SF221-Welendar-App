ac1ed45aaedf78c8ff4a70b13f619d41e772594d
bhuwich <phuwich.winyutrakul@gmail.com>
2022-04-18 18:17:09 +0700

    update Calendar -week/month/year

--- src/components/organism/Year.js
+++ src/components/organism/Year.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import { CalendarList, LocaleConfig} from 'react-native-calendars';
+import {View, Text, StyleSheet, Button, Alert, TouchableOpacity, SafeAreaView } from 'react-native';
+import {Box, NativerBaseProvider} from "native-base";
+import Icon from 'react-native-vector-icons/AntDesign';
+import IconName from 'react-native-vector-icons/Ionicons';
+import {Calendar, CalendarList, Agenda, LocaleConfig} from 'react-native-calendars';
 
-class Year extends React.Component {
+class Homepage extends React.Component {
     render(){
         LocaleConfig.locales['fr'] = {
             monthNames: [
@@ -23,39 +27,60 @@ class Year extends React.Component {
             dayNamesShort: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
             today: "Today"
         };
-        console.log('welcome');
         LocaleConfig.defaultLocale = 'fr';
         return(
-            <CalendarList
-                theme={{
-                    'stylesheet.calendar.header': {
-                        header: {
-                            justifyContent: 'flex-start',
-                            marginBottom: 10,
-                            marginLeft: 10
-                        },
-                        monthText: {
-                            fontSize: 22,
-                            fontWeight: 'bold',
-                            color:'#2F2E41'
-                        },
-                        dayHeader: {
-                            marginTop: 10,
-                            fontWeight: 'bold',
-                            color: '#2F2E41'
-                        }
-                        },
-                    todayBackgroundColor: '#FF975C',
-                    todayTextColor:'white',
-                    textDayFontWeight: 'bold',
-                    dayTextColor: '#2F2E41'
-            }}
-                hideArrows
-                enableSwipeMonths
-            >
-            </CalendarList>
+            <View style={{flex:1,backgroundColor: '#FFFFFF'}}>
+                <View style={{
+
+                    backgroundColor: '#FFFFFF',
+                    width:'100%',
+                    height:'100%'
+
+                }}>
+                    <View >
+                        <CalendarList
+                            style={{marginLeft:5 ,marginRight:5,marginTop:50}}
+                            theme={{
+                                'stylesheet.calendar.header': {
+                                    header: {
+                                        justifyContent: 'flex-start',
+                                        marginTop: 6,
+                                        marginBottom: 10,
+                                        marginLeft: 17
+                                    },
+                                    monthText: {
+                                        fontSize: 22,
+                                        fontWeight: 'bold',
+                                        color:'#2F2E41'
+                                    },
+                                    dayHeader: {
+                                        marginLeft:10,
+                                        width:25,
+                                        marginTop: 10,
+                                        fontWeight: 'bold',
+                                        color: '#2F2E41'
+                                    }
+                                },
+                                todayBackgroundColor: '#FF975C',
+                                todayTextColor:'white',
+                                textDayFontWeight: 'bold',
+                                dayTextColor: '#2F2E41'
+                            }}
+                            hideArrows
+
+                            enableSwipeMonths
+
+                        >
+
+                        </CalendarList>
+                    </View>
+
+                </View>
+
+
+            </View>
+
         );
     }
 }
-
-export default Year;
+export default Homepage;
